cd602747f7d4d601f208d830ab1849da52426be9
H-Scorpion <57944687+H-Scorpion@users.noreply.github.com>
2021-01-21 18:27:51 +0800

    build frontend

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,7 @@ const upload = multer({ storage: fileStorage, fileFilter: fileFilter });
 
 app.use(bodyParser.json());
 app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(express.static("public"))
 
 //set headers
 app.use((req, res, next) => {
---
+++ backend/public/precache-manifest.2beda6b56991d7126899b98c6184c969.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5f08d4c363b513050a05b4ac9aa6dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7308608e97d3dcb51e3e",
+    "url": "/static/css/main.831349ee.chunk.css"
+  },
+  {
+    "revision": "8314efd66c87ebe9cb76",
+    "url": "/static/js/2.54fabcb8.chunk.js"
+  },
+  {
+    "revision": "06236f42a9781d97809cba117aebbe7a",
+    "url": "/static/js/2.54fabcb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7308608e97d3dcb51e3e",
+    "url": "/static/js/main.7dcea2ff.chunk.js"
+  },
+  {
+    "revision": "09f84651e1e30890f51c",
+    "url": "/static/js/runtime-main.114c921f.js"
+  },
+  {
+    "revision": "add335bb6b7bc7c8e0c90ee601c1e36d",
+    "url": "/static/media/food_upscaled.add335bb.png"
+  },
+  {
+    "revision": "bc11db055c1ac7c180cb1b3d98e4a3d5",
+    "url": "/static/media/hamburger.bc11db05.jpg"
+  }
+]);
